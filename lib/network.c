ccc054a02311f3f6303dc00bf53211149b4580be
Rafael Zalamena <rzalamena@opensourcerouting.org>
2021-05-11 00:07:08 -0300

    lib: implement packet assembly functions
    Handle the assembly of fragmented IP packets.
    
    Cases covered:
    
     * Out of order fragments
     * Filter packets with data sum bigger than 65k
     * Filter packets with more than 113 fragments (see comments for details)
     * Ignore fragment overlaps (potential packet modification)
     * Limit the amount of total parallel packets assembly
     * Register memory usage with `show memory`
    
    Cases not covered:
    
     * IP options
    
    Reading material:
    
     * RFC 791 Internet Protocol
     * RFC 815 IP Datagram Reassembly Algorithms
     * RFC 4963 IPv4 Reassembly Errors at High Data Rates

--- lib/network.c
+++ lib/network.c
@@ -21,8 +21,13 @@
 
 #include <zebra.h>
 #include "log.h"
+#include "memory.h"
+#include "monotime.h"
 #include "network.h"
 #include "lib_errors.h"
+#include "checksum.h"
+#include "openbsd-queue.h"
+#include "thread.h"
 
 /* Read nbytes from fd and store into ptr. */
 int readn(int fd, uint8_t *ptr, int nbytes)
@@ -120,4 +125,616 @@ float htonf(float host)
 float ntohf(float net)
 {
 	return htonf(net);
+}
+
+/*
+ * IPv4 fragmentation handling.
+ */
+
+DEFINE_MTYPE_STATIC(LIB, PACKET_FRAGMENT, "IP packet fragment");
+DEFINE_MTYPE_STATIC(LIB, PACKET_DATA, "IP packet assembled data");
+
+/** A packet fragment holder data structure. */
+struct packet_fragment {
+	/** List pointer. */
+	SLIST_ENTRY(packet_fragment) entry;
+	/** Data offset. */
+	uint16_t offset;
+	/** Fragment size. */
+	uint16_t size;
+	/** Fragment data (allocated dinamically during *alloc). */
+	uint8_t data[];
+};
+
+/** Packet fragment list definition. */
+SLIST_HEAD(packet_fragment_list, packet_fragment);
+
+/**
+ * IP packet identification.
+ *
+ * In order for a fragment to belong to this packet it must match
+ * the following criteria: it must have the same source address,
+ * destination address, identification and protocol.
+ *
+ * RFC 791 Section 3.2. Discussion.
+ */
+struct ipv4_identification {
+	/** Identification field. */
+	uint16_t id;
+	/** Protocol. */
+	uint16_t protocol;
+	/** Source address. */
+	uint32_t source;
+	/** Destination address. */
+	uint32_t destination;
+};
+
+enum ip_packet_flag {
+	/** Data structure is in use. */
+	IP_PACKET_IN_USE = (1 << 0),
+	/** Currently being used to assemble IPv4 packet. */
+	IP_PACKET_IPV4 = (1 << 1),
+	/** Packet is complete. */
+	IP_PACKET_COMPLETE = (1 << 2),
+	/**
+	 * Ignore this packet:
+	 * This packet has been flagged to be ignored for one on the reasons:
+	 *  1. Its too fragmented so only account it once for statistics.
+	 *  2. Its a huge packet and should not be accounted more than once.
+	 *  3. Its a repeated packet.
+	 *
+	 *  The packet will be ignored until it ceases its activity and gets
+	 *  cleaned up by the periodic timer.
+	 */
+	IP_PACKET_IGNORE = (1 << 3),
+};
+
+/**
+ * Data structure that symbolizes the entire packet.
+ *
+ * It holds all fragments and can be used to build the whole packet.
+ */
+struct ip_packet {
+	/** Packet identification. */
+	struct ipv4_identification id;
+	/** Amount of fragments. */
+	uint16_t total_fragments;
+	/** Data structure flags. \see ip_packet_flag. */
+	uint16_t flags;
+	/** Current amount of fragmented data. */
+	uint32_t current_size;
+	/** Total packet size. */
+	uint32_t total_size;
+	/** Final header size. */
+	uint32_t header_size;
+	/** Fragments list. */
+	struct packet_fragment_list fragments;
+	/** Last access. */
+	time_t last_usage;
+	/** Packet complete data. */
+	uint8_t *data;
+};
+
+/** Encapsulation ToS value. */
+#define IPV4_ENCAP_TOS 0xC0
+/** Encapsulation magic value. */
+#define IP_ENCAP_MAGIC_VALUE 0x676F6C64
+
+
+static struct ip_packet packet_list[PACKET_ASSEMBLY_IN_PROGRESS_MAX];
+static struct thread_master *packet_thread;
+static struct thread *packet_cleanup_timer;
+static struct thread *packet_cleanup_event;
+struct ip_packet_statistics ip_packet_stats;
+
+/**
+ * Statistic counters for IP assembly/fragmentation.
+ */
+struct ip_encap_packet_statistics {
+	/** Invalid IP header version. */
+	uint64_t invalid_version;
+	/** Invalid header length: less than 5 octets (or 20 bytes). */
+	uint64_t invalid_header_length;
+	/** Invalid packet length: header says X but packet data is Y. */
+	uint64_t invalid_packet_length;
+	/** Invalid checksum. */
+	uint64_t invalid_checksum;
+	/** IP encapsulation fragment detected: no fragmentation used. */
+	uint64_t fragmented;
+	/** Invalid IP encapsulation version. */
+	uint64_t invalid_encapsulation_version;
+	/** Invalid IP encapsulation magic number. */
+	uint64_t invalid_encapsulation_magic;
+	/** Valid packets. */
+	uint64_t valid_packets;
+} ip_encap_packet_stats;
+
+/** Free packet resources. */
+static void ip_packet_reset(struct ip_packet *packet)
+{
+	struct packet_fragment *pf;
+
+	/* Free resources. */
+	while ((pf = SLIST_FIRST(&packet->fragments)) != NULL) {
+		SLIST_REMOVE(&packet->fragments, pf, packet_fragment, entry);
+		XFREE(MTYPE_PACKET_FRAGMENT, pf);
+	}
+	XFREE(MTYPE_PACKET_DATA, packet->data);
+
+	/* Reset variables. */
+	memset(packet, 0, sizeof(*packet));
+}
+
+/** Clean up a packet after usage. */
+static int ip_packet_cleanup_event(struct thread *t)
+{
+	struct ip_packet *packet = THREAD_ARG(t);
+
+	ip_packet_reset(packet);
+
+	return 0;
+}
+
+/** Gets an existing context or an unused one. */
+static struct ip_packet *ip_packet_find(const struct ipv4_header *ipv4)
+{
+	struct ip_packet *packet = NULL;
+	int first_available = -1;
+	int index;
+
+	for (index = 0; index < PACKET_ASSEMBLY_IN_PROGRESS_MAX; index++) {
+		/* Figure out the first available slot or just skip. */
+		if (!(packet_list[index].flags & IP_PACKET_IN_USE)) {
+			if (first_available != -1)
+				continue;
+
+			first_available = index;
+			continue;
+		}
+
+		/* Skip used IPv6 packet. */
+		if (!(packet_list[index].flags & IP_PACKET_IPV4))
+			continue;
+
+		/* Match identification. */
+		if (ipv4->id != packet_list[index].id.id)
+			continue;
+		if (ipv4->protocol != packet_list[index].id.protocol)
+			continue;
+		if (ipv4->source != packet_list[index].id.source)
+			continue;
+		if (ipv4->destination != packet_list[index].id.destination)
+			continue;
+
+		packet = &packet_list[index];
+		break;
+	}
+	if (packet)
+		return packet;
+
+	if (first_available == -1)
+		return NULL;
+
+	/* Use the new context. */
+	packet = &packet_list[first_available];
+	packet->flags = IP_PACKET_IN_USE | IP_PACKET_IPV4;
+	packet->id.id = ipv4->id;
+	packet->id.protocol = ipv4->protocol;
+	packet->id.source = ipv4->source;
+	packet->id.destination = ipv4->destination;
+
+	return packet;
+}
+
+/** Allocates memory for the whole packet and assemble it. */
+static void ip_packet_assemble(struct ip_packet *packet)
+{
+	struct packet_fragment *pf, *pfn;
+	struct ipv4_header *ipv4;
+	size_t offset = 0;
+
+	/* Allocate packet data and assemble it. */
+	packet->data = XMALLOC(MTYPE_PACKET_DATA,
+			       packet->header_size + packet->total_size);
+	SLIST_FOREACH_SAFE (pf, &packet->fragments, entry, pfn) {
+		memcpy(packet->data + offset, pf->data, pf->size);
+		offset += pf->size;
+
+		SLIST_REMOVE(&packet->fragments, pf, packet_fragment, entry);
+		XFREE(MTYPE_PACKET_FRAGMENT, pf);
+	}
+
+	/* Fix final IP header fields. */
+	ipv4 = (struct ipv4_header *)packet->data;
+	ipv4->total_length =
+		(uint16_t)(packet->header_size + packet->total_size);
+	ipv4->fragmentation = 0;
+	ipv4->checksum = 0;
+}
+
+/** Allocates memory for fragment and register it. */
+static void ip_packet_add_fragment(struct ip_packet *packet,
+				   uint16_t header_length, uint16_t offset,
+				   bool more_fragments, const void *data,
+				   uint16_t datalen)
+{
+	struct packet_fragment *pf;
+	struct packet_fragment *pfpos, *pfprev;
+	uint16_t offset_end;
+
+	/*
+	 * Find fragment position.
+	 *
+	 * Handling:
+	 *
+	 *  * Fragment already exists:
+	 *    The fragment is probably duplicated. If we want to be paranoiac
+	 *    we could `memcmp` the data to detect tampering attempts.
+	 *
+	 *  * Fragment overlaps:
+	 *    abort, it is possible someone is trying to tamper with our data.
+	 *
+	 *  * First fragment:
+	 *    Just accept it.
+	 *
+	 * `pfpos` is the current iteration position and `pflast` is the last
+	 * greater element before current. So if `pflast == NULL` insert at the
+	 * head of the list, otherwise after `pflast`.
+	 */
+	pfpos = NULL;
+	pfprev = NULL;
+	SLIST_FOREACH (pfpos, &packet->fragments, entry) {
+		/* Repeated packet: do nothing. */
+		if (pfpos->offset == offset)
+			return;
+		/* Higher offset, we want to insert before it. */
+		if (pfpos->offset > offset)
+			break;
+
+		/* Keep the current position. */
+		pfprev = pfpos;
+		continue;
+	}
+
+	/*
+	 * Detect overlaps.
+	 *
+	 * Don't attempt to create a fragment it will make the size check fail
+	 * (sum of all fragments) and it is probably not intended.
+	 */
+	if (pfprev) {
+		if (pfprev->offset)
+			offset_end = pfprev->offset + pfprev->size;
+		else
+			offset_end = pfprev->size - header_length;
+
+		/* Previous fragment exceeds the current fragment. */
+		if (offset_end > offset) {
+			ip_packet_stats.fragment_overlap++;
+			return;
+		}
+	}
+
+	/* Detect packets that are quickly accumulating more than expected. */
+	if ((packet->current_size + datalen) > IPV4_MAXIMUM_PACKET_SIZE) {
+		packet->flags |= IP_PACKET_IGNORE;
+		ip_packet_stats.huge_packets++;
+		return;
+	}
+
+	/* Allocate resources. */
+	pf = XCALLOC(MTYPE_PACKET_FRAGMENT, sizeof(*pf) + datalen);
+	pf->offset = offset;
+	pf->size = datalen;
+	memcpy(pf->data, data, datalen);
+
+	/* Insert item in the correct position. */
+	if (pfprev)
+		SLIST_INSERT_AFTER(pfprev, pf, entry);
+	else
+		SLIST_INSERT_HEAD(&packet->fragments, pf, entry);
+
+	/* Update packet counters. */
+	packet->total_fragments++;
+	packet->current_size += datalen;
+	if (!more_fragments) {
+		packet->total_size = offset + datalen;
+		/* Detect abnormally big packets (e.g. bigger than 65k). */
+		if (packet->total_size > IPV4_MAXIMUM_PACKET_SIZE) {
+			packet->flags |= IP_PACKET_IGNORE;
+			ip_packet_stats.huge_packets++;
+			return;
+		}
+	}
+
+	packet->last_usage = monotime(NULL);
+
+	/* Check for completion. */
+	if (packet->total_size
+	    && packet->total_size
+		       == (packet->current_size - packet->header_size)) {
+		ip_packet_assemble(packet);
+		packet->flags |= IP_PACKET_COMPLETE;
+		ip_packet_stats.assembled_packets++;
+	}
+}
+
+enum ip_packet_assemble_result ipv4_packet_assemble(const uint8_t *data,
+						    size_t datalen,
+						    const uint8_t **packetp,
+						    size_t *packetlen)
+{
+	const struct ipv4_header *ipv4 = (const struct ipv4_header *)data;
+	struct ip_packet *packet;
+	uint16_t fragment_length;
+	uint16_t header_length;
+	int checksum;
+
+	*packetp = NULL;
+	*packetlen = 0;
+
+	/* Basic check: data size is at least the header. */
+	if (datalen < sizeof(struct ipv4_header)) {
+		ip_packet_stats.invalid_header_length++;
+		return IPA_INVALID_HEADER_LENGTH;
+	}
+
+	/* Check version. */
+	if (ipv4_version(ipv4) != 4) {
+		ip_packet_stats.invalid_version++;
+		return IPA_INVALID_VERSION;
+	}
+
+	/* Check header length. */
+	header_length = (uint16_t)ipv4_header_length(ipv4);
+	if (header_length < 20) {
+		ip_packet_stats.invalid_header_length++;
+		return IPA_INVALID_HEADER_LENGTH;
+	}
+
+	/* Verify checksum. */
+	checksum = in_cksum(data, header_length);
+	if (checksum) {
+		ip_packet_stats.invalid_checksum++;
+		return IPA_INVALID_CHECKSUM;
+	}
+
+	/* Check packet length. */
+	fragment_length = ntohs(ipv4->total_length);
+	if (datalen < fragment_length) {
+		ip_packet_stats.invalid_packet_length++;
+		return IPA_INVALID_LENGTH;
+	}
+
+	/* Check if this packet is fragmented. */
+	if (ipv4_dont_fragment(ipv4)
+	    || (!ipv4_more_fragments(ipv4)
+		&& ipv4_fragment_offset(ipv4) == 0)) {
+		*packetp = data;
+		*packetlen = datalen;
+		ip_packet_stats.whole_packets++;
+		return IPA_NOT_FRAGMENTED;
+	}
+
+	/* Find an existing context or start a new one. */
+	packet = ip_packet_find(ipv4);
+	if (packet == NULL) {
+		ip_packet_stats.too_many_packets++;
+		return IPA_NO_MEMORY;
+	}
+
+	/* Don't attempt to assemble or account this packet anymore. */
+	if (packet->flags & IP_PACKET_IGNORE)
+		return IPA_IGNORED;
+
+	/*
+	 * Someone is trying to DoS us, so don't allocate more memory and wait
+	 * them to go away. The periodic timer will clean up this memory later.
+	 */
+	if (packet->total_fragments > IPV4_MAXIMUM_FRAMENTS_AMOUNT) {
+		packet->flags |= IP_PACKET_IGNORE;
+		ip_packet_stats.too_many_fragments++;
+		return IPA_TOO_MANY_FRAGMENTS;
+	}
+
+	/*
+	 * Attempt to receive and assemble packet fragments.
+	 *
+	 * If the packet was already assembled it means we've got a repeated
+	 * fragment and we should not bother the caller with it.
+	 */
+	if (!(packet->flags & IP_PACKET_COMPLETE)) {
+		/*
+		 * If not the first fragment, then skip header. Otherwise
+		 * keep and account it.
+		 *
+		 * The first fragment contains the IP header with the options
+		 * (if any) so we must account it and ignore the others.
+		 */
+		if (ipv4_fragment_offset(ipv4) > 0) {
+			data += header_length;
+			fragment_length -= header_length;
+		} else
+			packet->header_size = header_length;
+
+		ip_packet_add_fragment(
+			packet, header_length, ipv4_fragment_offset(ipv4) << 3,
+			ipv4_more_fragments(ipv4), data, fragment_length);
+		if (!(packet->flags & IP_PACKET_COMPLETE))
+			return IPA_OK_INCOMPLETE;
+	} else {
+		ip_packet_stats.repeated_packet++;
+		return IPA_REPEATED_PACKET;
+	}
+
+	*packetp = packet->data;
+	*packetlen = packet->header_size + packet->total_size;
+
+	/* Schedule clean up. */
+	thread_add_event(packet_thread, ip_packet_cleanup_event, packet, 0,
+			 &packet_cleanup_event);
+
+	return IPA_OK;
+}
+
+/** Removes completed and used packets. */
+static int ip_packet_periodic(struct thread *t __attribute__((unused)))
+{
+	time_t now;
+	int index;
+
+	/* Cache current time. */
+	now = monotime(NULL);
+
+	for (index = 0; index < PACKET_ASSEMBLY_IN_PROGRESS_MAX; index++) {
+		/* Skip unused slots. */
+		if (!(packet_list[index].flags & IP_PACKET_IN_USE))
+			continue;
+
+		/* Skip packet with recent activity. */
+		if (packet_list[index].last_usage + IP_PACKET_INACTIVE_INTERVAL
+		    >= now)
+			continue;
+
+		ip_packet_reset(&packet_list[index]);
+	}
+
+	/* Schedule next periodic clean up. */
+	thread_add_timer(packet_thread, ip_packet_periodic, NULL,
+			 IP_PACKET_INACTIVE_INTERVAL, &packet_cleanup_timer);
+
+	return 0;
+}
+
+void ip_fragmentation_handler_init(struct thread_master *tm)
+{
+	packet_thread = tm;
+	thread_add_timer(packet_thread, ip_packet_periodic, NULL,
+			 IP_PACKET_INACTIVE_INTERVAL, &packet_cleanup_timer);
+}
+
+const char *ip_packet_assemble_result_str(enum ip_packet_assemble_result result)
+{
+#define MATCH_RETURN(value)                                                    \
+	case (value):                                                          \
+		return #value
+
+	switch (result) {
+		MATCH_RETURN(IPA_OK);
+		MATCH_RETURN(IPA_OK_INCOMPLETE);
+		MATCH_RETURN(IPA_INVALID_VERSION);
+		MATCH_RETURN(IPA_INVALID_HEADER_LENGTH);
+		MATCH_RETURN(IPA_INVALID_LENGTH);
+		MATCH_RETURN(IPA_INVALID_CHECKSUM);
+		MATCH_RETURN(IPA_FRAGMENT_OVERLAP);
+		MATCH_RETURN(IPA_NO_MEMORY);
+		MATCH_RETURN(IPA_TOO_MANY_FRAGMENTS);
+		MATCH_RETURN(IPA_PACKET_TOO_BIG);
+		MATCH_RETURN(IPA_NOT_FRAGMENTED);
+		MATCH_RETURN(IPA_REPEATED_PACKET);
+		MATCH_RETURN(IPA_IGNORED);
+	default:
+		return "unknown";
+	}
+}
+
+enum ip_encap_packet_assemble_result
+ipv4_encap_parse(const void *data, size_t datalen, struct ipv4_encap_result *rv)
+{
+	struct ipv4_encap_header *ipv4e = (struct ipv4_encap_header *)data;
+	uint16_t header_length;
+	uint16_t packet_length;
+	int checksum;
+
+	/* Reset result data structure. */
+	memset(rv, 0, sizeof(*rv));
+
+	/* Basic check: data size is at least the header. */
+	if (datalen < sizeof(*ipv4e)) {
+		ip_encap_packet_stats.invalid_header_length++;
+		return IEPA_INVALID_HEADER_LENGTH;
+	}
+
+	/* Check version. */
+	if (ipv4_version(&ipv4e->ipv4) != 4) {
+		ip_encap_packet_stats.invalid_version++;
+		return IEPA_INVALID_VERSION;
+	}
+
+	/* Check header length. */
+	header_length = (uint16_t)ipv4_header_length(&ipv4e->ipv4);
+	if (header_length < 20) {
+		ip_encap_packet_stats.invalid_header_length++;
+		return IEPA_INVALID_HEADER_LENGTH;
+	}
+
+	/* Verify checksum. */
+	checksum = in_cksum(data, header_length);
+	if (checksum) {
+		ip_encap_packet_stats.invalid_checksum++;
+		return IEPA_INVALID_CHECKSUM;
+	}
+
+	/* Check packet length. */
+	packet_length = ntohs(ipv4e->ipv4.total_length);
+	if (datalen < packet_length) {
+		ip_packet_stats.invalid_packet_length++;
+		return IEPA_INVALID_LENGTH;
+	}
+
+	/* Check if this packet is fragmented. */
+	if (ipv4_more_fragments(&ipv4e->ipv4)
+	    || ipv4_fragment_offset(&ipv4e->ipv4)) {
+		ip_encap_packet_stats.fragmented++;
+		return IEPA_FRAGMENTED;
+	}
+
+	/* Encapsulation check: version. */
+	if (ipv4e->version != 0) {
+		ip_encap_packet_stats.invalid_encapsulation_version++;
+		return IEPA_INVALID_ENCAPSULATION_VERSION;
+	}
+	/* Encapsulation check: magic number. */
+	if (ipv4e->magic != htonl(IP_ENCAP_MAGIC_VALUE)) {
+		ip_encap_packet_stats.invalid_encapsulation_magic++;
+		return IEPA_INVALID_ENCAPSULATION_MAGIC;
+	}
+
+	/* Set and return results. */
+	rv->ifindex = ntohs(ipv4e->ifindex);
+	rv->protocol = ipv4e->ipv4.protocol;
+	rv->encap_length = header_length + IP_ENCAP_DATA_SIZE;
+	rv->destination = ipv4e->ipv4.destination;
+
+	/* Statistics. */
+	ip_encap_packet_stats.valid_packets++;
+
+	return IEPA_OK;
+}
+
+void ipv4_encap_output(const struct ipv4_encap_params *params, void *data,
+		       size_t datalen)
+{
+	struct ipv4_encap_header *ipv4e = data;
+
+	ipv4_set_version(&ipv4e->ipv4);
+	/*
+	 * NOTE: the encapsulation header does not count the encapsulation
+	 *       data as part of the header.
+	 */
+	ipv4_set_header_length(&ipv4e->ipv4, sizeof(ipv4e->ipv4));
+	ipv4e->ipv4.tos = IPV4_ENCAP_TOS;
+	ipv4e->ipv4.id = htons(((uint16_t)frr_weak_random()));
+	ipv4e->ipv4.total_length = htons(sizeof(ipv4e) + datalen);
+	ipv4e->ipv4.ttl = 1;
+	ipv4e->ipv4.protocol = params->protocol;
+	ipv4e->ipv4.source = params->source;
+	ipv4e->ipv4.destination = htonl(IPV4_ENCAP_DST);
+	ipv4e->ipv4.checksum = 0;
+	ipv4e->ipv4.checksum =
+		(uint16_t)in_cksum(ipv4e, ipv4_header_length(&ipv4e->ipv4));
+
+	ipv4e->version = 0;
+	ipv4e->ifindex = htons((uint16_t)params->ifindex);
+	ipv4e->magic = htonl(IP_ENCAP_MAGIC_VALUE);
 }
--- lib/network.h
+++ lib/network.h
@@ -26,6 +26,11 @@
 extern "C" {
 #endif
 
+#include <arpa/inet.h>
+
+#include <stdbool.h>
+#include <stdint.h>
+
 /* Both readn and writen are deprecated and will be removed.  They are not
    suitable for use with non-blocking file descriptors.
  */
@@ -62,6 +67,314 @@ static inline long frr_weak_random(void)
 	/* coverity[dont_call] */
 	return random();
 }
+
+/*
+ * IP fragmentation/assembly API.
+ */
+
+/**
+ * RFC 791 Section 3.1. Internet Header Format.
+ *
+ * Hosts must be prepared to accept datagrams of up to 576 octets.
+ */
+#define IPV4_MINIMUM_FRAGMENT_SIZE 576
+
+/**
+ * Maximum allowed IPv4 packet size (reassembled).
+ */
+#define IPV4_MAXIMUM_PACKET_SIZE 65535
+
+/**
+ * Maximum amount of fragments allowed to be queued for a single IPv4 packet.
+ *
+ * Reasoning:
+ *
+ *  Maximum packet size
+ * --------------------- = 113.77
+ * Minimum fragment size
+ */
+#define IPV4_MAXIMUM_FRAMENTS_AMOUNT 113
+
+/**
+ * Maximum number of allowed in progress fragmented packets.
+ */
+#define PACKET_ASSEMBLY_IN_PROGRESS_MAX 12
+
+/**
+ * Maximum inactive interval before getting free'd.
+ */
+#define IP_PACKET_INACTIVE_INTERVAL 4
+
+/**
+ * Possible IP assembly outcomes.
+ */
+enum ip_packet_assemble_result {
+	/** Packet was successfully assembled. */
+	IPA_OK = 0,
+	/** Packet is still being assembled. */
+	IPA_OK_INCOMPLETE,
+	/** Invalid IP packet version. */
+	IPA_INVALID_VERSION,
+	/** Packet part has invalid length. */
+	IPA_INVALID_HEADER_LENGTH,
+	/** Packet part has invalid length. */
+	IPA_INVALID_LENGTH,
+	/** Packet checksum invalid. */
+	IPA_INVALID_CHECKSUM,
+	/** Fragment overlapped existing part. */
+	IPA_FRAGMENT_OVERLAP,
+	/** Not enough resources to reassemble packet. */
+	IPA_NO_MEMORY,
+	/** Too many IP fragments. */
+	IPA_TOO_MANY_FRAGMENTS,
+	/** Packet is too big (>65k) and can't be assembled. */
+	IPA_PACKET_TOO_BIG,
+	/** Not fragmented. */
+	IPA_NOT_FRAGMENTED,
+	/** Packet already got read. */
+	IPA_REPEATED_PACKET,
+	/** Ignored packet (its too big or too fragmented). */
+	IPA_IGNORED,
+};
+
+/**
+ * Possible IP encapsulation parse outcomes.
+ */
+enum ip_encap_packet_assemble_result {
+	/** Packet was successfully assembled. */
+	IEPA_OK = 0,
+	/** Invalid IP packet version. */
+	IEPA_INVALID_VERSION,
+	/** Packet part has invalid length. */
+	IEPA_INVALID_HEADER_LENGTH,
+	/** Packet part has invalid length. */
+	IEPA_INVALID_LENGTH,
+	/** Packet checksum invalid. */
+	IEPA_INVALID_CHECKSUM,
+	/** Fragmented encapsulated packet. */
+	IEPA_FRAGMENTED,
+	/** Invalid encapsulation version. */
+	IEPA_INVALID_ENCAPSULATION_VERSION,
+	/** Invalid encapsulation magic. */
+	IEPA_INVALID_ENCAPSULATION_MAGIC,
+};
+
+/**
+ * Statistic counters for IP assembly/fragmentation.
+ */
+struct ip_packet_statistics {
+	/** Invalid IP header version. */
+	uint64_t invalid_version;
+	/** Invalid header length: less than 5 octets (or 20 bytes). */
+	uint64_t invalid_header_length;
+	/** Invalid packet length: header says X but packet data is Y. */
+	uint64_t invalid_packet_length;
+	/** Invalid checksum. */
+	uint64_t invalid_checksum;
+	/** IP fragment overlaps detected: possible malicious user. */
+	uint64_t fragment_overlap;
+	/**
+	 * We exceeded the amount of parallel packets being assembled.
+	 *
+	 * This is possible in two situations:
+	 *  1. We are receiving too much traffic and most of it is fragmented.
+	 *  2. We are being DDoS with fragmented packets using randomized IDs.
+	 */
+	uint64_t too_many_packets;
+	/**
+	 * Packets with fragment count exceeding IPV4_MAXIMUM_FRAMENTS_AMOUNT.
+	 */
+	uint64_t too_many_fragments;
+	/** Packets that were not fragmented. */
+	uint64_t whole_packets;
+	/** Total amount of assembled packets. */
+	uint64_t assembled_packets;
+	/**
+	 * Amount of packets repeated.
+	 *
+	 * This situation is either:
+	 *  1. When repeated fragments were received.
+	 *  2. We exceeded the transfer rate causing ID collision.
+	 */
+	uint64_t repeated_packet;
+	/** Amount of packets that exceeded 65k size. */
+	uint64_t huge_packets;
+};
+
+extern struct ip_packet_statistics ip_packet_stats;
+
+/**
+ * Parses and assembles the passed IPv4 packet (if fragmented).
+ *
+ * NOTE:
+ * Don't keep the pointer to `packet` because it will be `free()`d once the
+ * FRR thread scheduler is called.
+ *
+ * \param data the IPv4 packet raw data (with header).
+ * \param datalen the IPv4 packet read length as returned by syscall.
+ * \param packet pointer to return the assembled packet (only set if return
+ *               value is `IPA_OK`).
+ * \param packetlen pointer to return total packet length (header + data).
+ * \returns one of `enum ip_packet_assemble_result` values.
+ */
+enum ip_packet_assemble_result ipv4_packet_assemble(const uint8_t *data,
+						    size_t datalen,
+						    const uint8_t **packet,
+						    size_t *packetlen);
+
+/** Converts enum value to string. */
+const char *
+ip_packet_assemble_result_str(enum ip_packet_assemble_result result);
+
+/* Forward declaration. */
+struct thread_master;
+
+/** Initialize packet assembler API/periodic timers.  */
+void ip_fragmentation_handler_init(struct thread_master *tm);
+
+/*
+ * IP encapsulation.
+ */
+
+/** Build OSPF with special IP encapsulation. */
+#define OSPF_IP_ENCAP
+
+/* Special IP encapsulation protocols. */
+#define OSPF_IP_ENCAP_SPF       248
+#define OSPF_IP_ENCAP_OTHER     249
+#define OSPF_IP_ENCAP_DR        250
+
+/** Encapsulation parse results.  */
+struct ipv4_encap_result {
+	/** Interface index the packet came from. */
+	int32_t ifindex;
+	/** Encapsulation header length. */
+	uint16_t encap_length;
+	/** Encapsulation protocol. */
+	uint8_t protocol;
+	/** Destination address header value. */
+	uint32_t destination;
+};
+
+/**
+ * Parses a raw packet and returns whether the encapsulated data is valid or
+ * not.
+ *
+ * \param data raw packet pointer.
+ * \param datalen raw packet length.
+ * \param rv the results output.
+ * \returns `IEPA_OK` on success otherwise one of the codes in
+ *          `enum ip_encap_packet_assemble_result`.
+ */
+enum ip_encap_packet_assemble_result
+ipv4_encap_parse(const void *data, size_t datalen,
+		 struct ipv4_encap_result *rv);
+
+/** Encapsulation parameters. */
+struct ipv4_encap_params {
+	/** Source address for the packet. */
+	uint32_t source;
+	/** Interface index. */
+	int32_t ifindex;
+	/** Protocol. */
+	uint8_t protocol;
+};
+
+/**
+ * Encapsulation data size.
+ *
+ * Use this value to reserve the appropriated buffer size to use with
+ * `ip_encap_output`.
+ */
+#define IPV4_ENCAP_DATA_SIZE 28
+
+/** Encapsulation destination address. */
+#define IPV4_ENCAP_DST 0x7F8201FE
+
+/**
+ * Generates an encapsulation header and sets it in the packet pointed by
+ * `data`.
+ *
+ * \param params the encapsulation data parameters.
+ * \param data pointer to the data packet.
+ * \param datalen the amount of data in the packet.
+ */
+void ipv4_encap_output(const struct ipv4_encap_params *params, void *data,
+		       size_t datalen);
+
+/**
+ * Regular IPv4 header (simplified) from RFC 791 Section 3.1..
+ */
+struct ipv4_header {
+	uint8_t version_ihl;
+#define IPV4_VERSION 4
+	uint8_t tos;
+	uint16_t total_length;
+	uint16_t id;
+	uint16_t fragmentation;
+#define IPV4_MORE_FRAGMENTS 0x2000
+#define IPV4_DONT_FRAGMENT 0x4000
+	uint8_t ttl;
+	uint8_t protocol;
+	uint16_t checksum;
+	uint32_t source;
+	uint32_t destination;
+};
+
+static inline uint8_t ipv4_version(const struct ipv4_header *ipv4)
+{
+	return ipv4->version_ihl >> 4;
+}
+
+static inline void ipv4_set_version(struct ipv4_header *ipv4)
+{
+	ipv4->version_ihl = (ipv4->version_ihl & 0x0F) | (4 << 4);
+}
+
+static inline uint8_t ipv4_header_length(const struct ipv4_header *ipv4)
+{
+	return (ipv4->version_ihl & 0x0F) << 2;
+}
+
+static inline void ipv4_set_header_length(struct ipv4_header *ipv4,
+					  size_t header_length)
+{
+	ipv4->version_ihl = (ipv4->version_ihl & 0xF0) | (header_length >> 2);
+}
+
+static inline bool ipv4_more_fragments(const struct ipv4_header *ipv4)
+{
+	return (ntohs(ipv4->fragmentation) & IPV4_MORE_FRAGMENTS)
+	       == IPV4_MORE_FRAGMENTS;
+}
+
+static inline bool ipv4_dont_fragment(const struct ipv4_header *ipv4)
+{
+	return (ntohs(ipv4->fragmentation) & IPV4_DONT_FRAGMENT)
+	       == IPV4_DONT_FRAGMENT;
+}
+
+static inline uint16_t ipv4_fragment_offset(const struct ipv4_header *ipv4)
+{
+	return ntohs(ipv4->fragmentation) & 0x1FFF;
+}
+
+/* Magic encapsulation header. */
+struct ipv4_encap_header {
+	struct ipv4_header ipv4;
+
+	/** Encapsulation version. */
+	uint16_t version;
+	/** Interface index. */
+	uint16_t ifindex;
+	/** Magic version. */
+	uint32_t magic;
+};
+
+
+/** Encapsulation data length. */
+#define IP_ENCAP_DATA_SIZE                                                     \
+	(sizeof(uint16_t) + sizeof(uint16_t) + sizeof(uint32_t))
 
 #ifdef __cplusplus
 }
