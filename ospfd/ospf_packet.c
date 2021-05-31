0cf01a4fb6796e4f8786ac5bd6ee3b20f5620cb4
Renato Westphal <renato@opensourcerouting.org>
2021-05-31 10:27:51 -0300

    ospfd: rename the graceful restart header
    Both the GR helper code and the upcoming GR restarting code are going
    to share a lot of definitions. As such, rename ospf_gr_helper.h to
    ospf_gr.h, which will be the central point of all GR definitions
    and prototypes.
    
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

--- ospfd/ospf_packet.c
+++ ospfd/ospf_packet.c
@@ -55,7 +55,7 @@
 #include "ospfd/ospf_dump.h"
 #include "ospfd/ospf_errors.h"
 #include "ospfd/ospf_zebra.h"
-#include "ospfd/ospf_gr_helper.h"
+#include "ospfd/ospf_gr.h"
 
 /*
  * OSPF Fragmentation / fragmented writes
