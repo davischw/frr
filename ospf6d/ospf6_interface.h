d49ced9ba1ebf05b308836d30f2f5fff81449a3f
David Lamparter <equinox@opensourcerouting.org>
2021-07-28 11:11:35 +0200

    ospf6d: allow creating interface w/o LSDBs
    To support virtual links, it makes sense to put a dummy interface into
    the backbone area, which virtual links are neighbors on.  That interface
    shouldn't ever have its various LSDBs used for anything, so the "basic"
    create/delete added here doesn't create them.
    
    (cherry picked from commit 48f21d9ef63de94fd07427d81f7fce7a459231f5)

--- ospf6d/ospf6_interface.h
+++ ospf6d/ospf6_interface.h
@@ -205,6 +205,10 @@ ospf6_interface_lookup_by_ifindex(ifindex_t, vrf_id_t vrf_id);
 extern struct ospf6_interface *ospf6_interface_create(struct interface *);
 extern void ospf6_interface_delete(struct ospf6_interface *);
 
+/* "basic" interface = used for virtual links */
+extern struct ospf6_interface *ospf6_interface_basic_create(struct interface *);
+extern void ospf6_interface_basic_delete(struct ospf6_interface *);
+
 extern void ospf6_interface_enable(struct ospf6_interface *);
 extern void ospf6_interface_disable(struct ospf6_interface *);
 
