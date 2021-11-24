613310f89b79986b27f9afd01c8d67c85431b9c5
Rafael Zalamena <rzalamena@opensourcerouting.org>
2021-11-24 14:49:19 -0300

    bgpd: fix address list BFD integration
    Notify the API about addresses change.
    
    Issue: 199

--- bgpd/bgp_bfd.c
+++ bgpd/bgp_bfd.c
@@ -170,7 +170,35 @@ void bgp_peer_bfd_update_source(struct peer *p)
 
 	/* Update peer's source/destination addresses. */
 	bfd_sess_addresses(session, &family, &src.v6, &dst.v6);
-	if (family == AF_INET) {
+	if (family != p->su.sa.sa_family) {
+		if (BGP_DEBUG(bfd, BFD_LIB)) {
+			if (family == AF_INET)
+				zlog_debug(
+					"%s: address [%pI4->%pI4] to [%pI6->%pI6]",
+					__func__, &src.v4, &dst.v4,
+					source ? &source->sin6.sin6_addr
+					       : &src.v6,
+					&p->su.sin6.sin6_addr);
+			else
+				zlog_debug(
+					"%s: address [%pI6->%pI6] to [%pI4->%pI4]",
+					__func__, &src.v6, &dst.v6,
+					source ? &source->sin.sin_addr
+					       : &src.v4,
+					&p->su.sin.sin_addr);
+		}
+
+		if (p->su.sa.sa_family == AF_INET)
+			bfd_sess_set_ipv4_addrs(
+				session, source ? &source->sin.sin_addr : NULL,
+				&p->su.sin.sin_addr);
+		else
+			bfd_sess_set_ipv6_addrs(session,
+						source ? &source->sin6.sin6_addr
+						       : NULL,
+						&p->su.sin6.sin6_addr);
+		changed = true;
+	} else if (family == AF_INET) {
 		if ((source && source->sin.sin_addr.s_addr != src.v4.s_addr)
 		    || p->su.sin.sin_addr.s_addr != dst.v4.s_addr) {
 			if (BGP_DEBUG(bfd, BFD_LIB))
