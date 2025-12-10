a999eee87404f3a06eabcb06390928cd78384b72
David Schweizer <dschweizer@opensourcerouting.org>
2025-12-10 12:31:00 +0100

    test commit, ignore
    Signed-off-by: David Schweizer <dschweizer@opensourcerouting.org>

--- staticd/static_gr_vty.c
+++ staticd/static_gr_vty.c
@@ -46,13 +46,14 @@
 /* clang-format off */
 
 
-DEFPY(staticd_show_graceful_restart, staticd_show_graceful_restart_cmd,
-      "show static graceful-restart [vrf]$vrf_name [json]",
-      SHOW_STR
-      STATICD_STR
-      "GRACEFUL_RESTART_STR"
-      VRF_STR
-      JSON_STR)
+DEFPY( staticd_show_graceful_restart,
+       staticd_show_graceful_restart_cmd,
+       "show static graceful-restart [vrf$vrf_name] [json]",
+       SHOW_STR
+       STATICD_STR
+       "GRACEFUL_RESTART_STR"
+       VRF_STR
+       JSON_STR)
 {
 	vrf_id_t vrf_id;
 
